dfc221eb2b8604065156dde64f166f64ac6106a9
phadhiil001 <phadhiil001@gmail.com>
2024-01-16 11:51:47 -0600

    commit

--- class_work/Web Dev 2 Winter 2024/Module 2/_2 PHP & Forms Starting Files/PHP & Forms Starting Files/thankyou.php
+++ class_work/Web Dev 2 Winter 2024/Module 2/_2 PHP & Forms Starting Files/PHP & Forms Starting Files/thankyou.php
@@ -3,16 +3,23 @@
     if(isset($_POST['fname'])) {
         $content = "Thank you for your submission, {$_POST['fname']}.";
     }
+
+    // Validate the student number
+    function filterinput() {
+        return filter_input(INPUT_POST, 'studentnum', FILTER_VALIDATE_INT);
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <title>Thank you for your submission</title>
+    <link rel="stylesheet" type="text/css" href="formstyle.css" />
 </head>
 <body>
     <div id="wrapper">
         <h4><?= $content ?></h4>
+        <?php if(filterinput()): ?>
         <table>
             <tr>
                 <td>Name:</td>
@@ -28,14 +35,24 @@
             </tr>
             <?php 
                 // if the user chose BIT or BA show their major
-                if ($_POST['program' == "BIT"]) {
+                if ($_POST['program'] == "BIT") {
                     $majorContent = $_POST['bitmajor'];
                 }
                 elseif ($_POST['program'] == "BA") {
                     $majorContent = $_POST['bamajor'];
                 }
+
+                if (isset($majorContent)):
             ?>
+                <tr>
+                    <td>Major: </td>
+                    <td><?= $majorContent ?></td>
+                </tr>
+            <?php endif ?>
         </table>
+        <?php else: ?>
+            <h4>You did not supply an appropriate student nunmber</h4>
+        <?php endif ?>
     </div>
 </body>
 </html>
