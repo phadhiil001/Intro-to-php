63137bef1d6e09f7b0cc18138c8865ca21273c72
phadhiil001 <phadhiil001@gmail.com>
2024-01-27 16:30:36 -0600

    new file:   class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/__MACOSX/._db_connect.php 	new file:   class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/__MACOSX/._insert.php 	new file:   class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/__MACOSX/._nav.php 	new file:   class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/__MACOSX/._select.php 	new file:   class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/__MACOSX/._select_where.php 	new file:   class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/__MACOSX/._styles.css 	new file:   class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/__MACOSX/._update.php 	new file:   class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/db_connect.php 	new file:   class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/insert.php 	new file:   class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/nav.php 	new file:   class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/select.php 	new file:   class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/select_where.php 	new file:   class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/styles.css 	new file:   class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/update.php

---
+++ class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/insert.php
@@ -0,0 +1,54 @@
+<?php
+
+    /*
+    difference between include and require
+    if you use include and there is a problem, such asa maybe the page does not exist, the page will continue loading 
+    for require, none of the below code will execute if the 'db_connect.php' doesn't work.
+
+    include  - it tries to find and runs the file even if there are errors in that file. It continues executing the rest of the script.
+    require ('db_connect.php'); // Fatal error - script can not continue without this file
+
+    For include, all the below code will still be executed even though there is an issue with db_connect.php
+    */
+
+    require('db_connect.php');
+    
+    if ($_POST && !empty($_POST['author']) && !empty($_POST['content'])) {
+        //  Sanitize user input to escape HTML entities and filter out dangerous characters.
+        $author = filter_input(INPUT_POST, 'author', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $content = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        
+        //  Build the parameterized SQL query and bind to the above sanitized values.
+        $query = "INSERT INTO quotes (author, content) VALUES (:author, :content)";
+        $statement = $db->prepare($query);
+
+        
+        //  Bind values to the parameters
+        $statement->bindValue(':author', $author);
+        $statement->bindValue(':content', $content);
+        
+        //  Execute the INSERT.
+        //  execute() will check for possible SQL injection and remove if necessary
+        if($statement->execute()) {
+            echo "Success";
+        }
+
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PDO Insert</title>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+    <?php include('nav.php'); ?>
+    <form method="post" action="insert.php">
+        <label for="author">Author</label>
+        <input id="author" name="author">
+        <label for="content">Content</label>
+        <input id="content" name="content">
+        <input type="submit">
+    </form>
+</body>
+</html>
---
+++ class_work/Web Dev 2 Winter 2024/Module 3/3 CRUD Starting Files/select_where.php
@@ -0,0 +1,38 @@
+<?php
+    require('db_connect.php');
+    
+    // Build and prepare SQL String with :id placeholder parameter. 
+    // use LIMIT 1 to avoid  multiple rows if more than one row matches the WHERE clause and prevent  unexpected results.
+    $query = "SELECT * FROM quotes WHERE id = :id LIMIT 1";
+    $statement = $db->prepare($query);
+    
+    // Sanitize $_GET['id'] to ensure it's a number.
+    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+    
+    // Bind the :id parameter in the query to the sanitized
+    // $id specifying a binding-type of Integer.
+
+    //  PDO::PARAM_INT - the sql datatype integer, it is used for integers, it throws error  if value passed is not integer.
+    $statement->bindValue('id', $id, PDO::PARAM_INT);
+    $statement->execute();
+    
+    // Fetch the row selected by primary key id.
+    $row = $statement->fetch();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PDO SELECT with WHERE</title>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+    <?php include('nav.php'); ?>
+    <h1>Rows Found: <?= $statement->rowCount() ?></h1>
+    <h2>Content</h2>
+    <p><?= $row['content'] ?></p>
+    <h2>Author</h2>
+    <p><?= $row['author'] ?></p>
+    <h2>Zero Rows Found?</h2>
+    <p>Try <a href="?id=1">this</a> link and then play with the GET parameter in the URL.</p>
+</body>
+</html> 
